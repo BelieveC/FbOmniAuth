2fae772303d7a10889f2f16537b5e177c6be5935
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-29 21:48:34 +0530

    Routes Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  get 'auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  get 'sessions/destroy'
+    resources :sessions, only: [:create, :destroy]
+    resource :home, only: [:show]
 
-  get 'home/show'
-
+    root to: "home#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
