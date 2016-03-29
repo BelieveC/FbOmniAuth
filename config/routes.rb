b5f8541a9f659d61e7bd45689b951095e23b6d79
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-29 21:47:54 +0530

    Add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'home/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
